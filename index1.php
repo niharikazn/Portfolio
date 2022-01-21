0d8963526c7cd715d5599e663b0ff9d5ca492ecd
niharikazn <79219977+niharikazn@users.noreply.github.com>
2022-01-21 22:27:11 +0530

    Update index1.php

--- index1.php
+++ index1.php
@@ -1,29 +1,4 @@
 <?php
 header("Location:thankyou3.html");
-$connectionInfo = array("UID" => "Niharika", "pwd" => "Niha1412", "Database" => "Humanity", "LoginTimeout" => 30, "Encrypt" => 1, "TrustServerCertificate" => 0);
-$serverName = "tcp:portfolioniha.database.windows.net,1433";
-$conn = sqlsrv_connect($serverName, $connectionInfo);
 
-// Create connection
-$conn = new mysqli($servername, $username, $password, $dbname);
-// Check connection
-if ($conn->connect_error) {
-  die("Connection failed: " . $conn->connect_error);
-}
-   
-$name=$_POST["name"];
-$email=$_POST["email"];
-$sub=$_POST["Subject"];
-$me=$_POST["mesage"];
-$sql = "INSERT INTO `contact` (`name`, `email`,`subject`,`mesage`)
-VALUES ('$name','$email','$sub','$me')";
-
-if ($conn->query($sql) === TRUE) {
-//echo"success";
-header("Location:thankyou3.html");
-} else {
-  echo "Error: " . $sql . "<br>" . $conn->error;
-}
-
-$conn->close();
 ?>
