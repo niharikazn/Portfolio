ac2f7c33428c16b84087e23cbf5ef4d2a7466095
niharikazn <79219977+niharikazn@users.noreply.github.com>
2022-01-21 08:22:50 +0530

    Update index1.php

--- index1.php
+++ index1.php
@@ -1,24 +1,10 @@
 <?php
 
+$con=mysqli_init();
 
-header("Location:thankyou3.html");
-
-try {
-    $conn = new PDO("sqlsrv:server = tcp:humanityser.database.windows.net,1433; Database = Humanity", "CloudSA616db234", "10A16F35K8281J8Q$");
-    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-}
-catch (PDOException $e) {
-    print("Error connecting to SQL Server.");
-    die(print_r($e));
-}
-
-// SQL Server Extension Sample Code:
-$connectionInfo = array("UID" => "CloudSA616db234", "pwd" => "10A16F35K8281J8Q$", "Database" => "Humanity", "LoginTimeout" => 30, "Encrypt" => 1, "TrustServerCertificate" => 0);
-$serverName = "tcp:humanityser.database.windows.net,1433";
-$conn = sqlsrv_connect($serverName, $connectionInfo);
-?>
+mysqli_real_connect($conn, "humanityy.mysql.database.azure.com", "niharika@humanityy", "12345Niha", "Humanity", 3306);
 // Create connection
-$conn = new mysqli($servername, $username, $password, $dbname);
+//$conn = new mysqli("tcp:humanityser.database.windows.net,1433", $username, $password, $dbname);
 // Check connection
 if ($conn->connect_error) {
   die("Connection failed: " . $conn->connect_error);
